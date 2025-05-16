667683b0f49958cef948197cbc3515404cac7d4d
Juxarius <exdarkrb@gmail.com>
2025-05-16 09:52:56 +0800

    Updated command to also log for approved server

--- utils.py
+++ utils.py
@@ -66,11 +66,12 @@ def requires_approved(func):
     @functools.wraps(func)
     async def wrapper(ctx: ApplicationContext, *args, **kwargs):
         server_data = config['approvedServers'].get(str(ctx.guild_id), None)
+        cmd = f'/{ctx.command} ' + ' '.join(str(v) for v in kwargs.values())
         if server_data is None:
-            cmd = f'/{ctx.command} ' + ' '.join(str(v) for v in kwargs.values())
             warning(f"{ctx_info(ctx)} Unapproved server sent {cmd}")
             await ctx.respond("This server is not approved to use this command.")
             return
+        info(f"{ctx_info(ctx)} Approved server sent {cmd}")
         ctx.server_data = server_data
         return await func(ctx, *args, **kwargs)
     return wrapper
